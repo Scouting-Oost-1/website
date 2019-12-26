0a713120efe37e58db19963c2b2f27699871cc4d
Floris Jansen <florismartijnjansen@gmail.com>
2019-12-26 14:24:26 +0100

    add page styling

--- page.php
+++ page.php
@@ -4,7 +4,15 @@
 <?php get_header(); ?>
 <?php if(have_posts()) : while(have_posts()) : the_post(); ?>
 
-  <main class="page" id="site-content">
+  <?php if ( has_post_thumbnail() ) : ?>
+    <header class="page-thumb page-thumb--<?php the_field('palette_color') ?>">
+      <?php the_post_thumbnail('post-thumbnail',
+        array( 'class' => 'page-thumb__img')
+      ); ?>
+    </header>
+  <?php endif; ?>
+
+  <main class="page <?php the_field('palette_color') ?>" id="site-content">
 
     <h1><?php the_title(); ?></h1>
     <?php the_content(); ?>
