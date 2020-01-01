6c244de127197b63a1de59001c0dfafc00dfcfe1
Floris Jansen <florismartijnjansen@gmail.com>
2020-01-01 20:47:28 +0100

    fill & style frontpage

--- front-page.php
+++ front-page.php
@@ -46,41 +46,82 @@
 
 
 
-    <section class="past--front">
-      <h1>Blijf op de hoogte</h1>
+    <section class="past">
+      <h1 class="past__title">Blijf op de hoogte</h1>
 
-      <div class="news--front">
+      <?php
+        $recent_news = get_posts(array(
+          'numberposts' => 1,
+          'category' => 'nieuws'
+        ));
+        $recent_news = $recent_news[0];
+        $recent_news_ID = $recent_news->ID;
+
+        if ($recent_news->post_type):
+      ?>
+
+      <div class="past__part">
         <h2>Nieuws</h2>
+        <p>Lees het laatste nieuwsbericht</p>
 
-        <article class="message--front">
-          [Thumbnail]
-          <h3>[Titel]</h3>
-          [Excerpt]
-          Lees verder...
+        <article class="past__item">
+          <?php echo get_the_post_thumbnail($recent_news_ID, 'medium'); ?>
+          <h3 class="past__post-title">
+            <a href="<?php the_permalink($recent_news_ID); ?>">
+              <?php echo $recent_news->post_title; ?>
+            </a>
+          </h3>
+          <?php echo $recent_news->post_excerpt; ?>
         </article>
       </div>
 
-      <div class="photos--front">
+      <?php endif; ?>
+
+
+
+      <!-- <div class="past__part photos--front">
         <h2>Foto’s</h2>
-        <article class="photoalbum--front">
+        <p>Bekijk het nieuwste fotoalbum</p>
+
+        <article class="past__item">
           [Fotoalbum]
         </article>
-      </div>
+      </div> -->
 
-      <div class="lvs--front">
+
+
+      <?php
+        $recent_lv_query = array(
+          'post_type' => 'publicaties',
+          'posts_per_page' => 1,
+          'category' => 'nieuws'
+        );
+        $recent_lv = new WP_Query($recent_lv_query);
+
+        if ($recent_lv->have_posts()): $recent_lv->the_post();
+      ?>
+
+      <div class="past__part lvs--front">
         <h2>Lopend Vuurtje</h2>
-        <article class="lv--front">
-          Bekijk de laatste editie van ons clubblad (januari)
+        <p>Lees de laatste editie van ons clubblad</p>
+        <article class="past__item">
+          <a href="<?php the_field('pdf'); ?>" target="_blank">
+            <?php the_post_thumbnail('medium'); ?>
+            <h3 class="past__post-title"><?php the_title(); ?></h3>
+          </a>
         </article>
       </div>
+
+      <?php endif; wp_reset_postdata();?>
+
     </section>
 
 
 
-    <section class="members--front">
+    <section class="front-members">
       <h1>Jeugdleden</h1>
-      <div class="speltakken--front">
-        <table>
+      <div class="front-members__cont">
+        <table class="front-members__speltakken">
           <thead>
             <th>Leeftijd</th>
             <th>Meisjes</th>
@@ -89,40 +130,94 @@
           <tbody>
             <tr>
               <td>4–7</td>
-              <td>Bevers</td>
+              <td>
+                <a href="<?php echo get_permalink(
+                  get_page_by_title( 'Bevers' ) ); ?>">
+                  Bevers
+                </a>
+              </td>
             </tr>
             <tr>
               <td>7–11</td>
-              <td>Kabouters</td>
-              <td>Welpen</td>
+              <td>
+                <a href="<?php echo get_permalink(
+                  get_page_by_title( 'Kabouters' ) ); ?>">
+                  Kabouters
+                </a>
+              </td>
+              <td>
+                <a href="<?php echo get_permalink(
+                  get_page_by_title( 'Welpen' ) ); ?>">
+                  Welpen
+                </a>
+              </td>
             </tr>
             <tr>
               <td>11–15</td>
-              <td>Gidsen</td>
-              <td>Verkenners</td>
+              <td>
+                <a href="<?php echo get_permalink(
+                  get_page_by_title( 'Gidsen' ) ); ?>">
+                  Gidsen
+                </a>
+              </td>
+              <td>
+                <a href="<?php echo get_permalink(
+                  get_page_by_title( 'Verkenners' ) ); ?>">
+                  Verkenners
+                </a>
+              </td>
             </tr>
             <tr>
               <td>15–18</td>
-              <td>Explorers</td>
+              <td>
+                <a href="<?php echo get_permalink(
+                  get_page_by_title( 'Explorers' ) ); ?>">
+                  Explorers
+                </a>
+              </td>
             </tr>
           </tbody>
         </table>
+
+        <div class="front-members__buttons">
+          <a class="button" href="<?php echo get_permalink(
+            get_page_by_title( 'Ledenadministratie' ) ); ?>?Actie=Aanmelden">
+            Word lid</a>
+          <a class="button button--light" href="<?php echo get_permalink(
+            get_page_by_title( 'Ledenadministratie' ) ); ?>?Actie=Wijzigen">
+            Uitschrijven</a>
+          <a class="button button--light" href="<?php echo get_permalink(
+            get_page_by_title( 'Ledenadministratie' ) ); ?>?Actie=Afmelden">
+            Gegevens wijzigen</a>
+        </div>
       </div>
-      <a href="#" class="button">Word lid</a>
-      <a href="#" class="button button--light">Uitschrijven</a>
-      <a href="#" class="button button--light">Gegevens wijzigen</a>
     </section>
 
 
 
-    <section class="rest--front">
-      <h1>Over</h1>
-      [Inhoud over-pagina]
+    <section class="front-end">
+      <div class="front-end__cont">
+        <div class="front-end__text">
+          <?php the_content(); ?>
+        </div>
 
-      <a href="#" class="button">Huur onze gebouwen</a>
-      <a href="#" class="button">Steun Scouting Oost 1</a>
-      <a href="#" class="button">Bekijk de agenda</a>
-      <a href="#" class="button">Neem contact op</a>
+        <nav class="continue">
+          <h2>Ga verder...</h2>
+          <a class="button button--light" href="<?php echo get_permalink(
+            get_page_by_path( 'verhuur' ) ); ?>">
+            Huur onze gebouwen</a>
+          <a class="button button--light" href="<?php echo get_permalink(
+            get_page_by_path( 'steunen' ) ); ?>">
+            Steun Scouting Oost 1</a>
+          <a class="button button--light" href="<?php echo get_permalink(
+            get_page_by_path( 'agenda' ) ); ?>">
+            Bekijk de agenda</a>
+          <a class="button button--light" href="<?php echo get_permalink(
+            get_page_by_path( 'contact' ) ); ?>">
+            Neem contact op</a>
+        </nav>
+      </div>
+
     </section>
 
 
