59b794837c87b85f050ea4b60a1f7fa340e1d1a5
Floris Jansen <florismartijnjansen@gmail.com>
2020-02-06 22:51:20 +0100

    add page layout for publication

---
+++ single-publicaties.php
@@ -0,0 +1,43 @@
+<?php /**
+ * Single publicatie
+ */ ?>
+
+<?php get_header(); ?>
+<?php if(have_posts()) : while(have_posts()) : the_post(); ?>
+
+  <header class="page-thumb page-thumb--<?php the_field('palette_color') ?>">
+    <?php if ( has_post_thumbnail() ) : ?>
+      <?php the_post_thumbnail('post-thumbnail',
+        array( 'class' => 'page-thumb__img')
+      ); ?>
+    <?php else: ?>
+      <div class="page-thumb__spacer"></div>
+    <?php endif; ?>
+  </header>
+
+  <main class="photoalbum" id="site-content">
+
+    <h1><?php the_title(); ?></h1>
+    <?php the_content(); ?>
+
+    <p class="photoalbum__button-cont">
+      <button href="#" class="button" id="js-slideshow-button">
+        <i class="fas fa-download referral__icon"></i>
+        Download
+      </button>
+    </p>
+
+  </main>
+
+<?php endwhile; ?>
+
+<?php else: ?>
+
+  <main>
+
+    <p>Sorry, deze pagina lijkt niet te bestaan.</p>
+
+  </main>
+
+<?php endif; ?>
+<?php get_footer(); ?>
