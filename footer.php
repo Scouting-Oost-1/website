47da67fa0d77e92af0a478981e70cb18377e5a5a
Floris Jansen <florismartijnjansen@gmail.com>
2022-03-02 16:49:22 +0100

    add link to become friend to footer

--- footer.php
+++ footer.php
@@ -61,6 +61,14 @@
           <li>
             <a href="<?php echo get_permalink( get_page_by_path( 'steunen' ) ); ?>">
               Steun Scouting Oost 1</a>
+              <?php $administration_link = get_permalink(
+              get_page_by_path( 'vriend-worden' ) ); ?>
+            <ul>
+              <li>
+                <a href="<?php echo $friend_link; ?>">
+                  Word vriend</a>
+              </li>
+            </ul>
           </li>
           <li>
             <a href="<?php echo get_permalink( get_page_by_path( 'staf' ) ); ?>">
