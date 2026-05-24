2a64b571efafc59821bc9c8aef28673a23810b90
Floris Jansen <florismartijnjansen@gmail.com>
2026-05-24 17:04:44 +0200

    additional comment prevention methods

--- functions.php
+++ functions.php
@@ -111,20 +111,39 @@ remove_action( 'admin_print_styles', 'print_emoji_styles' );
 
 
 
+add_action('admin_init', function () {
+    // Redirect any user trying to access comments page
+    global $pagenow;
 
+    if ($pagenow === 'edit-comments.php') {
+        wp_safe_redirect(admin_url());
+        exit;
+    }
+
+    // Remove comments metabox from dashboard
+    remove_meta_box('dashboard_recent_comments', 'dashboard', 'normal');
+
+    // Disable support for comments and trackbacks in post types
+    foreach (get_post_types() as $post_type) {
+        if (post_type_supports($post_type, 'comments')) {
+            remove_post_type_support($post_type, 'comments');
+            remove_post_type_support($post_type, 'trackbacks');
+        }
+    }
+});
+
+// Close comments on the front-end
+add_filter('comments_open', '__return_false', 20, 2);
+add_filter('pings_open', '__return_false', 20, 2);
+
+// Hide existing comments
+add_filter('comments_array', '__return_empty_array', 10, 2);
 
 /* Remove comment support */
 // Removes from admin menu
 add_action( 'admin_menu', 'my_remove_admin_menus' );
 function my_remove_admin_menus() {
     remove_menu_page( 'edit-comments.php' );
-}
-// Removes from post and pages
-add_action('init', 'remove_comment_support', 100);
-
-function remove_comment_support() {
-    remove_post_type_support( 'post', 'comments' );
-    remove_post_type_support( 'page', 'comments' );
 }
 // Removes from admin bar
 function so1_admin_bar_render() {
